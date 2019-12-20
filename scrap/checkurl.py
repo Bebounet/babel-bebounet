0e01042d1f71f42426f4cabb6bb6a2edf69b7b78
Bebounet <nicosagnier@hotmail.fr>
2019-12-20 14:01:24 +0100

    update checkurl

--- scrap/checkurl.py
+++ scrap/checkurl.py
@@ -17,6 +17,15 @@ from bs4 import BeautifulSoup
 
 dataset = []
 
+"""
+
+soup.find("meta", property="og:title")
+soup.find("meta", property="og:description")
+soup.find("meta", property="og:image")
+soup.find("meta", property="og:url")
+
+"""
+
 
 def printseparator():
     """ Fonction qui affiche une ligne de séparation """
@@ -43,6 +52,7 @@ def get_urls(arglist, is_verbose=True):
         if html:
             displayurl(html, is_verbose)
             writetodict(html, is_verbose)
+    # count_dataset(dataset)
 
 
 def displayurl(html, is_verbose):
@@ -62,24 +72,31 @@ def displayurl(html, is_verbose):
 
 
 F_URL = "url"
-F_STATUS = "status_code"
-F_HTML = "content"
-F_TITLE = "title"
+F_STATUS = "Status_code"
+F_HTML = "Content"
+F_TITLE = "Title"
+F_IMAGE = "Image"
+F_DESC = "Description"
+F_COUNTER = "Nombre d'objet"
+F_DATASET = "Dataset"
 
 
 def writetodict(html, is_verbose=False):
     """ création d'un dictionnaire et ecriture de données dans un fichier 'checkurl.json' """
-    title = search_title(html.text)
-    dict = {
-        F_URL: html.url,
-        F_STATUS: html.status_code,
-        F_HTML: html.text[:6000],
-        F_TITLE: title,
-    }
+    # title = search_title(html.text)
+    # dict_hmtl = {
+    # F_URL: html.url,
+    # F_STATUS: html.status_code,
+    # F_HTML: html.text[:6000],
+    # F_TITLE: title,
+    # }
+    dict_meta = search_meta(html.text)
+    dict_hmtl = {F_STATUS: html.status_code}
+    dict_hmtl.update(dict_meta)
 
     # ATTENTION : dataset est défini en global comme une liste
     global dataset
-    dataset.append(dict)
+    dataset.append(dict_hmtl)
 
     # affiche le nom du fichier .py
     print(__file__)
@@ -90,11 +107,22 @@ def writetodict(html, is_verbose=False):
     # recupere le nom du fichier dans la configuration du ssysteme d'exploitation
     basedir = os.path.dirname(os.path.abspath(__file__))
     print(basedir)
+
+    dataset_api = {F_COUNTER: len(dataset), F_DATASET: dataset}
     # creation du fichier chekurl.json dans le répertoire scrap
     filename = basedir + "/" + "checkurl.json"
     with open(filename, "w", encoding="utf8") as f:
-        json.dump(dataset, f)
+        json.dump(dataset_api, f)
         print(f"file {filename} created !")
+
+
+# def count_dataset(data):
+#     """ Fornction qui permet de savoir combien d'objet il y a dans le dict """
+#     counter = len(data)
+#     print(50 * "***")
+#     print(counter)
+#     data_input = {F_COUNTER: counter}
+#     return data_input
 
 
 def search_title_by_bs4(text):
@@ -115,8 +143,46 @@ def search_title_by_bs4(text):
     return soup.title.string
 
 
+def search_meta(text):
+    soup = BeautifulSoup(text, "lxml")
+    # Recherche des données
+    # Verification qu'il y a bien un meta title sinon on recupere title sans passer par la fonction meta -> property
+    title = soup.find("meta", property="og:title")
+    if not title:
+        title_content = soup.title.string
+    else:
+        title_content = title["content"]
+    # Verification qu'il y a bien un meta description sinon on recupere description sans passer par la fonction meta -> property
+    desc = soup.find("meta", property="og:description")
+    desc_content = desc["content"] if desc else ""
+
+    # Verification qu'il y a bien un meta image sinon on recupere image sans passer par la fonction meta -> property
+    img = soup.find("meta", property="og:image")
+    img_content = img["content"] if img else ""
+
+    # Verification qu'il y a bien un meta url sinon on recupere url sans passer par la fonction meta -> property
+    url = soup.find("meta", property="og:url")
+    url_content = url["content"] if url else ""
+
+    # print(f"*****************title = {title}")
+    # print(f"*****************desc = {desc}")
+    # print(f"*****************img = {img}")
+    # print(f"*****************url = {url}")
+
+    # Création du dict
+    dict_with_img = {
+        F_TITLE: title_content,
+        F_DESC: desc_content,
+        F_IMAGE: img_content,
+        F_URL: url_content,
+    }
+    print(dict_with_img)
+    return dict_with_img
+
+
 def search_title(text):
     """ Fonction qui cherche le titre d'une page HTML """
+    search_meta(text)
     return search_title_by_bs4(text)
 
     # DEPRECATED USE BEATIFULL SOUP INSTEAD
@@ -140,12 +206,13 @@ if __name__ == "__main__":
 
     listedesurls = [
         # "http://localhost:8080/formext/avions/avions",
+        "https://openclassrooms.com/",
         "https://www.dealabs.com",
         "https://www.ouest-france.fr/",
+        "chrome-extension://aejoelaoggembcahagimdiliamlcdmfm/index.html#requests",
+        "https://www.youtube.com/",
+        "https://twitter.com/home",
     ]
     get_urls(listedesurls)
 
 print(len(dataset))
-
-with open("test.json", "w+", encoding="utf8") as f:
-    json.dump(dataset, f)
