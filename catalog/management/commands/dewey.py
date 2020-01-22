8c4963365308c42192b33a0e2b6fdaf9b5b22f69
Bebounet <nicosagnier@hotmail.fr>
2020-01-22 16:08:14 +0100

    deploy to heroku

---
+++ catalog/management/commands/dewey.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+
+from catalog.utils import xls_reader
+from catalog.models import DeweyTest
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--source", help=f"source file", default="scrap/La_Dewey_simplifiee.xls"
+        )
+
+    def handle(self, *args, **options):
+        source = "scrap/La_Dewey_simplifiee.xls"
+        if options["source"]:
+            source = options["source"]
+
+        rc = xls_reader(DeweyTest, source)
+        print(f"Nb of dewey items processed {rc}")
